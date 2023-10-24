44f00854e2ab2303b6b48e47c38272286029474d
Aman Singh <119008813+Atulmishra32@users.noreply.github.com>
2023-10-24 16:06:26 +0530

    updated

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
+// const display = document.querySelector("#result");
+// const buttons = document.querySelectorAll("button");
 
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else {
-      display.innerText += item.id;
-    }
-  };
-});
+const synth = window.speechSynthesis;
+function speak(val) {
+  let result = document.getElementById("result");
+  result.value += val;
+  if ("speechSynthesis" in window) {
+    var msg = new SpeechSynthesisUtterance(val);
+    window.speechSynthesis.speak(msg);
+  } else {
+    console.log("Speech synthesis not supported");
+  }
+}
+
+function clearResult() {
+  let result = document.getElementById("result");
+  result.value = "";
+}
+
+function backspace() {
+  let result = document.getElementById("result");
+  result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+  let result = document.getElementById("result");
+  let res = eval(result.value);
+  if ("speechSynthesis" in window) {
+    var msg = new SpeechSynthesisUtterance("The result is " + res);
+    window.speechSynthesis.speak(msg);
+  } else {
+    console.log("Speech synthesis not supported");
+  }
+  result.value = res;
+}
+
+// buttons.forEach((item) => {
+//   item.onclick = () => {
+//     if (item.id == "clear") {
+//       display.innerText = "";
+//     } else if (item.id == "backspace") {
+//       let string = display.innerText.toString();
+//       display.innerText = string.substr(0, string.length - 1);
+//     } else if (display.innerText != "" && item.id == "equal") {
+//       display.innerText = eval(display.innerText);
+//     } else if (display.innerText == "" && item.id == "equal") {
+//       display.innerText = "Empty!";
+//       setTimeout(() => (display.innerText = ""), 2000);
+//     } else {
+//       display.innerText += item.id;
+//     }
+//   };
+// });
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 
